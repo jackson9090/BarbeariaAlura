51a1f0108ef6309dddbdd76c011dcba8982bd0bc
jackson9090 <114963578+jackson9090@users.noreply.github.com>
2023-04-20 20:09:49 -0300

    .

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ squares.forEach(square => {
   square.addEventListener('click', () => {
     if (square.innerHTML === '') {
       square.innerHTML = player;
-      player = player === 'X' ? 'O' : 'X';
-    }
-  });
-});
-
-const squares = document.querySelectorAll('.square');
-let player = 'X';
-
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    if (square.innerHTML === '') {
-      square.innerHTML = player;
       if (checkWin()) {
         resetGame();
       } else {
